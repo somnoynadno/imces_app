c1c092e83f3af93f758a86256e7ebf1c4eb436cf
somnoynadno <somnoynadno@yandex.ru>
2019-12-13 23:21:22 +0700

    Login page added

--- app.py
+++ app.py
@@ -1,40 +1,75 @@
-from flask import abork
+from flask import abort
 from flask import Flask
 from flask import redirect
 from flask import request
 from flask import render_template
 from flask import session
+from flask import url_for
 
+import datetime
 import mysql.connector
+
 import matplotlib as plt
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='/static')
+
+app.config['SESSION_TYPE'] = 'memcached'
+app.config['SECRET_KEY'] = 'I hope this enough to be secret'
 
 
 @app.route('/', methods=['GET'])
-def hello():
+def index():
 	# TODO: main page
-    return "Fine"
+	if not 'username' in session:
+		return redirect(url_for('login'), 302)
+	else:
+		return "OK"
 
 
 @app.route('/login', methods=['GET', 'POST'])
-def login(name):
-	# TODO: return login page
-    return "Login"
+def login():
+	if request.method == 'POST':
+		username = request.form.get('username')
+		password = request.form.get('password')
+
+		if not username or not password:
+			return "Wrong credentials", 400
+
+		session['username'] = username
+
+		return redirect(url_for('index'), 302)
+	else:
+		return render_template('login.html')
 
 
 @app.route('/register', methods=['GET', 'POST'])
-def register(name):
+def register():
 	# TODO: return register page
     return "Register"
 
 
-@app.route('/<int:station_number>', methods=['GET'])
-def statistics(name):
-	# TODO: return statistics on station
-    return "Statistics"
+@app.route('/get_info/<int:station_number>', methods=['GET'])
+def statistics(station_number	):
+	conn = mysql.connector.connect(
+         user='Fomin',
+         password='VvbrKYKj',
+         host='imces.ru',
+         port=22303,
+         database='apik3')
+
+	cursor = conn.cursor()
+
+	query = "show tables;"
+	cursor.execute(query)
+
+	result = []
+	for line in cursor:
+		result.append(line)
+
+	conn.close()
+
+	return str(result)
 
 
 if __name__ == '__main__':
-    app.run()
-    
+    app.run(debug=True)
